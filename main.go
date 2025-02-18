37f1b99084b7f222a621a3d9fa4a07ca598e472d
thetsajeet <ajeet.tumuluru@gmail.com>
2025-02-18 03:14:54 +0000

    routing and middlewares

--- main.go
+++ main.go
@@ -1,17 +1,62 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
 	mux := http.NewServeMux()
-	server := http.Server{}
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
 
-	server.Handler = mux
-	server.Addr = ":8080"
+	apiCfg := apiConfig{
+		fileServerHits: atomic.Int32{},
+	}
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	filepathHandler := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInfo(filepathHandler))
+	mux.HandleFunc("GET /api/healthz", handlerHealthz)
+	mux.HandleFunc("GET /api/metrics", apiCfg.handlerFileServerHits)
+	mux.HandleFunc("POST /api/reset", apiCfg.handlerResetMetrics)
 
-	server.ListenAndServe()
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(server.ListenAndServe())
+}
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInfo(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Cache-Control", "no-cache")
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerFileServerHits(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileServerHits.Load())))
+	w.WriteHeader(http.StatusOK)
+}
+
+func (cfg *apiConfig) handlerResetMetrics(w http.ResponseWriter, r *http.Request) {
+	cfg.fileServerHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func handlerHealthz(w http.ResponseWriter, r *http.Request) {
+	r.Header.Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
